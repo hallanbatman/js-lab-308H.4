e26f338628ae79e638c46b337a63fcc24ba0c7cb
Hallan Batman <batman.hallan@gmail.com>
2025-08-06 19:55:00 -0400

    Part 3 - complete

--- README.md
+++ README.md
@@ -9,27 +9,27 @@
 
 ## ✅ Part 1: Refactoring Old Code
 
-## Part 2: Expanding Functionality
+## ✅ Part 2: Expanding Functionality
 
-- Declare a variable that stores the number of columns in each row of data within the CSV.
-- Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+- ✅ Declare a variable that stores the number of columns in each row of data within the CSV.
+- ✅ Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
 
-- Store your results in a two-dimensional array.
-    - Each row should be its own array, with individual entries for each column.
-    - Each row should be stored in a parent array, with the heading row located at index 0.
-- Cache this two-dimensional array in a variable for later use.
+- ✅ Store your results in a two-dimensional array.
+    - ✅ Each row should be its own array, with individual entries for each column.
+    - ✅ Each row should be stored in a parent array, with the heading row located at index 0.
+- ✅ Cache this two-dimensional array in a variable for later use.
 
-## Part 3: Transforming Data
+## ✅ Part 3: Transforming Data
 
 While the data is now much more workable than it was in its string format, there is still a large amount of obscurity in the data itself. When we access an arbitrary index of the results array, it is impossible to know what that data is referring to without additional cross-referencing.
 
 In order to make it more obvious what the data is, we will transform our rows into objects.
 
 Implement the following:
-- For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
-     - Convert these keys to all lowercase letters for consistency.
-- Store these objects in an array, in the order that they were originally listed.
-- Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
+- ✅ For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+     - ✅ Convert these keys to all lowercase letters for consistency.
+- ✅ Store these objects in an array, in the order that they were originally listed.
+- ✅ Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
 
 Important: While this functionality can be built into the original CSV parser you built in Part 2, we are intentionally creating two different algorithms to test different skillsets. Please leave these sections separate even if it would be more efficient to combine them.
 
--- script.js
+++ script.js
@@ -1,67 +1,89 @@
 // #### R-ALAB 308.4.1: | Working with Data Collections ####
 
+//##########################################################
+
 // ✅ # Part 1: Refactoring Old Code #
-// Part 2: Expanding Functionality #
 
-// Tasks:
-// Declare a variable that stores the number of columns in each row of data within the CSV.
-// Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+//##########################################################
 
-//CSV data:ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+// ✅ Part 2: Expanding Functionality #
+
+// ✅ Declare a variable that stores the number of columns in each row of data within the CSV.
+// ✅ Instead of hard-coding four columns per row, expand your code to accept any number of columns.
+// ✅ This should be calculated dynamically based on the first row of data.
+// ✅ Store your results in a two-dimensional array.
+// ✅ Each row should be its own array, with individual entries for each column.
+// ✅ Each row should be stored in a parent array, with the heading row located at index 0.
+// ✅ Cache this two-dimensional array in a variable for later use.
+
 let csv = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 
 // Refactoring individual variables for each cell, into creating an array of cells.
-let tempCells = []
-let permCells = []
+let table = []
+let cells = [""]
 let currentCell = 0;
-
-let cell1 = "";
-let cell2 = "";
-let cell3 = "";
-let cell4 = "";
 
 for (let i = 0; i < csv.length; i++) {// Loop through the characters of a given CSV string.
     let char = csv[i];
-    //console.log(csv[i]);
 
     if (char === ",") { //if comma, move to cell.
         currentCell++;
-        //console.log(`found comma`);
-        //console.log(`Char found before comma, added to currentCell = ${currentCell}`);
+        if (cells.length <= currentCell) {
+            cells.push("");// add new cell if it doesn't exist yet
+        }
     }
 
     //if new line, print the row and reset
     else if (char === "\n") {//console.log(`found line`);
-        tempCells.push(cell1, cell2, cell3, cell4); //push cells to Array tempCells
-        currentCell = 0; //Reset currentCell
-        permCells.push(tempCells); //Push tempCells to permCells
-        tempCells = []; //Reset Array tempCells
-        //Reset Object individual Cells
-        cell1 = "";
-        cell2 = "";
-        cell3 = "";
-        cell4 = "";
+        table.push([...cells]); //Push copy to table
+        cells = [""]//reset cells
+        currentCell = 0; //Reset currentCell    
     }
 
     else {// Otherwise, add char to the current cell
-        if (currentCell === 0) {
-            cell1 += char;
-        } else if (currentCell === 1) {
-            cell2 += char;
-        } else if (currentCell === 2) {
-            cell3 += char;
-        } else if (currentCell === 3) {
-            cell4 += char;
-        }
+        cells[currentCell] += char;
     }
 
-    if (csv.length - 1 == i) { // If last char in string print final cells
-    console.log(tempCells);
+    if (csv.length - 1 == i) { // If last char in string push final cells
+        table.push([...cells]);//tempCells.push(...cells);
     }
 }
 
-console.log(permCells);
-//console.log(tempCells);
+console.log(table);
+
+//##########################################################
+
+// ✅ Part 3: Transforming Data
+
+// ✅ For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+// ✅ - Convert these keys to all lowercase letters for consistency.
+// ✅ Store these objects in an array, in the order that they were originally listed.
+
+let headers = [];
+
+for (let i = 0; i < table[0].length; i++) {
+    //console.log(table[0].length)// How many items in the header
+    //console.log(table[0][i])// Header items
+    headers.push(table[0][i].toLowerCase());// Convert header items to lowercase
+}
+
+let rowObjects = [];
+
+for (let i = 1; i < table.length; i++) {
+    let row = table[i];
+    //console.log(table[i])// Row of objects
+    let obj = {};
+
+    for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = row[j];
+    }
+
+    rowObjects.push(obj);
+}
+
+console.log(rowObjects);
+
+//##########################################################
 
 // #### Previous Assigment | 308H.3 ####
 // Store each “cell” of data in a variable.
